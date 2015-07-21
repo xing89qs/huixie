e8cffe1b57b511f32c92f6cb791ee68b0fc7fab1
wanglizhi <729699036@qq.com>
2015-07-21 10:28:42 +0800

    add web chat controller

--- application/controllers/ta.php
+++ application/controllers/ta.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); //防止用户直接访问
 
-class TA extends CI_Controller {
+class Ta extends CI_Controller {
 	function __construct(){
 		parent::__construct();
 		$this->load->helper('url');
--- application/controllers/weixin.php
+++ application/controllers/weixin.php
@@ -1,9 +1,68 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); //防止用户直接访问
 
+define('TOKEN', 'fuxuejiaoyu')
 class Weixin extends CI_Controller {
 	function __construct(){
 		parent::__construct();
 		$this->load->helper('url');
+	}
+	//默认入口
+	function index(){
+		if(checkSignature() == false){
+			exit(0);
+		}
+		if($_GET['echostr']){
+			echo $_GET['echostr'];
+			exit(0);
+		}
+
+		$postData = $GLOBALS['HTTP_RAW_POST_DATA'];
+		if(!$postData){
+			echo 'wrong input';
+			exit(0);
+		}
+		$xmlObj = simplexml_load_string($postData, 'SimpleXMLElement',LIBXML_NOCDATA);
+		if(!xmlObj){
+			echo 'wrong input';
+			exit(0);
+		}
+		$fromUserName = $xmlObj->FromUserName;
+		$toUserName = $xmlObj->toUserName;
+		$msgType = $xmlObj->MsgType;
+		if('text' != $msgType){
+			$retMsg = '只关注文本消息';
+		}else{
+			$content = $xmlObj->Content;
+			$retMsg = $content;
+		}
+		$retTmp = "<xml>
+		<ToUserName><![CDATA[%s]]></ToUserName>
+		<FromUserName><![CDATA[%s]]></FromUserName>
+		<CreateTime>%s</CreateTime>
+		<MsgType><![CDATA[text]]></MsgType>
+		<Content><![CDATA[%s]]></Content>
+		<FuncFlag>0</FuncFlag>
+		</xml>";
+		$resultStr = sprintf($retTmp, $fromUserName, $toUserName, time(), $retMsg);
+		echo $resultStr
+	}
+	//验证签名
+	function checkSignature(){
+		$signature = $_GET['signature'];
+		$timestamp = $_POST['timestamp'];
+		$nonce = $_GET['nonce'];
+
+		$token = TOKEN;
+		$tmpArr = array($token, $timestamp, $nonce);
+		sort($tmpArr);
+		$tmpStr = implode($tmpArr);
+		$tmpStr = shal($tmpStr);
+
+		if($tmpStr == $signature){
+			return true;
+		}else{
+			return false;
+		}
 	}
 	function getAllFollower(){
 		$this->load->model('WeixinModel');
@@ -11,4 +70,25 @@ class Weixin extends CI_Controller {
 		echo $this->WeixinModel->getAllFollower();
 	}
 	
+
+	//获取IP
+	function getIp(){
+		if(isset($_SERVER)){
+			if(isset($_SERVER['HTTP_X_FORWARDED_FOR'])){
+				$realip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+			}else if(isset($_SERVER['HTTP_CLIENT_IP'])){
+				$realip = $_SERVER['HTTP_CLIENT_IP'];
+			}else{
+				$realip = $_SERVER['REMOTE_ADDR'];
+			}
+		}else{
+			if(getenv('HTTP_X_FORWARDED_FOR')){
+				$realip = getenv('HTTP_X_FORWARDED_FOR');
+			}else if(getenv('HTTP_CLIENT_IP')){
+				$realip = getenv('HTTP_CLIENT_IP');
+			}else{
+				$realip = getenv('REMOTE_ADDR');
+			}
+		}
+	}
 }
